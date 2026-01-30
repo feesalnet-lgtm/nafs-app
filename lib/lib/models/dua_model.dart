c0c4eb4bec744d9591f351920d277a18d2d04ae7
feesalnet-lgtm <fee.s.a.l.n.e.t@gmail.com>
2026-01-30 20:08:26 +0300

    Create Dua model class with required fields

---
+++ lib/lib/models/dua_model.dart
@@ -0,0 +1,15 @@
+class Dua {
+  final String title;
+  final String arabicText;
+  final String translation;
+  final String source;
+  final String category;
+
+  Dua({
+    required this.title,
+    required this.arabicText,
+    required this.translation,
+    required this.source,
+    required this.category,
+  });
+}
